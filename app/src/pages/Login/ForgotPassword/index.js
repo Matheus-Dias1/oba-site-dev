b94e0a8c87684ef45b29f89ea384bd6f3b6746ef
matheus-dias1 <reptarop@gmail.com>
2020-05-06 02:04:07 -0300

    empty list splash added on forgotpassword

--- app/src/pages/Login/ForgotPassword/index.js
+++ app/src/pages/Login/ForgotPassword/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Image
+} from 'react-native';
+import splash from '../../../assets/teste.png'
 
 
-export default function ForgotPassword(){
-    return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Forgot Password!</Text>
-      </View>
-  
-    );
+export default function ForgotPassword() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Image
+          source={splash}
+          style={{width: 100, height: 100, resizeMode:'center'}}
+        />
+        <Text style={{fontSize:16, fontWeight:'300', textAlign:'center',color:'#737380'}}>{'Não há nada\npara se ver aqui'}</Text>
+    </View>
+
+  );
 }
