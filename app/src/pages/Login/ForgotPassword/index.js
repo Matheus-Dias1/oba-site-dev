de497366d791536a828d127092daa0c760b0e0ae
matheus-dias1 <reptarop@gmail.com>
2020-05-08 20:03:32 -0300

    recover password screen added

--- app/src/pages/Login/ForgotPassword/index.js
+++ app/src/pages/Login/ForgotPassword/index.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
-  Image
+  TouchableWithoutFeedback,
+  TextInput,
+  Alert,
+  ActivityIndicator
 } from 'react-native';
-import splash from '../../../assets/teste.png'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+import api from '../../../services/api';
 
 
 export default function ForgotPassword() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       <Image
-          source={splash}
-          style={{width: 100, height: 100, resizeMode:'center'}}
-        />
-        <Text style={{fontSize:16, fontWeight:'300', textAlign:'center',color:'#737380'}}>{'Não há nada\npara se ver aqui'}</Text>
-    </View>
+  const [inputSelected, setInputSelected] = useState(false);
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigator = useNavigation();
 
+
+  async function handleRecoverPassword(){
+    if (loading) return;
+    if (!(email.includes('@') && email.includes('.'))) return;
+    setLoading(true);
+    try{
+      await api.post('recoverPassword', {email})
+      Alert.alert('Sucesso', 'O e-mail será enviado caso exista uma conta com o endereço informado')
+      navigator.goBack();
+    }catch(err){
+      Alert.alert('Erro ao recuperar senha', 'Houve um erro ao recuperar a sua senha, tente novamente mais tarde')
+    }finally{
+      setLoading(false);
+    }
+  }
+
+
+  return (
+    <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+      <View style={styles.content}> 
+      <Text style={styles.title}>Recuperar senha</Text>
+      <Text style={styles.bodyText}>Enviaremos um e-mail com uma nova senha gerada aleatóriamente para o e-mail que usou no cadastro da sua conta.</Text> 
+        <View style={inputSelected ? styles.focusedInputContainer : styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            value={email}
+            placeholder='E-mail'
+            keyboardType='email-address'
+            onChange={(e) => setEmail(e.nativeEvent.text)}
+            enablesReturnKeyAutomatically={true}
+            textContentType="password"
+            onFocus={() => setInputSelected(true)}
+            onBlur={() => setInputSelected(false)}
+          />
+        </View>
+      </View>
+        <TouchableWithoutFeedback onPress={() => handleRecoverPassword()}>
+          <View style={styles.recoverButton}>
+            <Text style={styles.buttonText}>Cadastrar</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        {loading && <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#000" />
+          </View>}
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
--- app/src/pages/Login/ForgotPassword/styles.js
+++ app/src/pages/Login/ForgotPassword/styles.js
@@ -2,5 +2,78 @@ import { StyleSheet } from 'react-native';
 
 
 export default StyleSheet.create({
-    
+    title: {
+        alignSelf: 'center',
+        fontSize: 25,
+        fontWeight: '500',
+        color: '#737380',
+        marginBottom: 10
+    },
+    inputContainer: {
+        height: 45,
+        marginHorizontal: 0,
+        marginTop: 20,
+        borderBottomWidth: 1,
+        borderColor: 'lightgray',
+        justifyContent: 'flex-end'
+    },
+    focusedInputContainer: {
+        height: 45,
+        marginHorizontal: 0,
+        marginTop: 20,
+        borderBottomWidth: 2,
+        borderColor: '#049434',
+        justifyContent: 'flex-end'
+    },
+    recoverButton: {
+        backgroundColor: '#049434',
+        borderRadius: 18,
+        height: 45,
+        width: 260,
+        marginHorizontal: 0,
+        marginTop: 40,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 30
+    },
+    textInput: {
+        marginHorizontal: 2,
+        marginVertical: 2,
+        fontSize: 18,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '500'
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    content: {
+        marginHorizontal: 25
+    },
+    bodyText: {
+        marginTop: 20,
+        color: '#737380',
+        fontSize: 17,
+        fontWeight: '300',
+        textAlign: 'justify'
+    },
+    loadingContainer: {
+        width: 70,
+        height: 70,
+        backgroundColor: '#ccc',
+        borderRadius: 12,
+        opacity: 0.7,
+        position: 'absolute',
+        right: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 0,
+        top: '45%',
+    },
 });
