12b597a5939134822364e72c162f15c9c019f693
matheus-dias1 <reptarop@gmail.com>
2020-05-23 23:51:10 -0300

    added pixel on the help app screen

--- app/src/pages/Profile/Help/index.js
+++ app/src/pages/Profile/Help/index.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   View,
   Text,
   TouchableWithoutFeedback,
   Linking,
   StatusBar,
+  AsyncStorage,
 } from 'react-native';
 import styles from './styles';
 import * as MailComposer from 'expo-mail-composer';
 import { Ionicons, FontAwesome } from '@expo/vector-icons/'
+import api from '../../../services/api'
 
-function openWhatsApp() {
-  Linking.openURL('whatsapp://send?phone=5534997762094')
-}
 
-function makeCall(){
-  Linking.openURL('tel:+5534997762094')
-}
-
-function sendEmail() {
-  MailComposer.composeAsync({
-    recipients: ['obahortifruti20@gmail.com']
-  })
-}
 
 export default function Help() {
+  useEffect(()=>{
+    const abortController = new AbortController();
+    return abortController.abort();
+  },[])
+  
+  function openWhatsApp() {
+    sendPixel('whatsapp')
+    Linking.openURL('whatsapp://send?phone=5534997762094')
+  }
+  
+  function makeCall() {
+    sendPixel('phone')
+    Linking.openURL('tel:+5534997762094')
+  }
+  
+  function sendEmail() {
+    sendPixel('email')
+    MailComposer.composeAsync({
+      recipients: ['obahortifruti20@gmail.com']
+    })
+  }
+  
+  async function sendPixel(method) {
+    
+    try {
+      await api.post('fbPixel/contact', {
+        email: await AsyncStorage.getItem('email'),
+        name: await AsyncStorage.getItem('name'),
+        method
+      }, {
+        headers: {
+          authorization: 'Bearer ' + await AsyncStorage.getItem('accessToken')
+        }
+      })
+    } catch (err) {
+
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="#f2f2f2"/>
+      <StatusBar backgroundColor="#f2f2f2" />
       <View style={styles.content}>
         <View>
           <Text style={styles.bodyText}>Entre em contato conosco por um dos meios listados abaixo.</Text>
