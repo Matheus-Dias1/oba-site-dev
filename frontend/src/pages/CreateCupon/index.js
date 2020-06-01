c870a49424ae0594f1aa43175053248d95995848
matheus-dias1 <reptarop@gmail.com>
2020-06-01 15:03:11 -0300

    fixed frontend menu

--- frontend/src/pages/CreateCupon/index.js
+++ frontend/src/pages/CreateCupon/index.js
@@ -74,7 +74,7 @@ export default function CreateCupon() {
                 <Link className='menu-link' to="/panel/purchases"><FaClipboardCheck size={16} color="FFFFFF" />Pedidos</Link>
                 <Link className='menu-link' to="/panel/products"><FaBoxOpen size={16} color="FFFFFF" />Produtos</Link>
                 <Link className='menu-link' to="/panel/reports"><FaChartLine size={16} color="FFFFFF" />Relatórios</Link>
-                <Link className='menu-link' to="/panel/cupon"><FaCalendarAlt size={16} color="FFFFFF" />Adicionar Horários</Link>
+                <Link className='menu-link' to="/panel/schedule"><FaCalendarAlt size={16} color="FFFFFF" />Adicionar Horários</Link>
                 <Link className='menu-link' to="/panel/cupon"><FaTags size={16} color="FFFFFF" />Criar cupom</Link>
                 <Link className='menu-link' to="/panel/push"><FaBell size={16} color="FFFFFF" />Enviar notificações</Link>
             </Menu>
